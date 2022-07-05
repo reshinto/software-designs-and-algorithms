77a1a7ebe34a6cf35fed833acac314b22bcc1af1
reshinto <terencekong2002@gmail.com>
2022-07-05 21:48:55 +0800

    Add building blocks of OOP part 2 hometask solution

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
@@ -2,10 +2,77 @@ import { Comparable } from './Comparable';
 
 let id = 0;
 
+const memo = {
+    counter: 0
+}
+
 export abstract class Item implements Comparable<Item> {
-    public compareTo(other: Item): number {
-        // your code goes here
-    }
+    private static numberOfItems: number = memo.counter;
+    private id: number;
+    private value: number;
+    private name: string;
+    private weight: number;
 
     // your code goes here
+    constructor(name: string, value: number, weight: number) {
+        this.id = memo.counter;
+        memo.counter++;
+        Item.numberOfItems = memo.counter;
+        this.name = name;
+        this.value = value;
+        this.weight = weight;
+    }
+
+    public compareTo(other: Item): number {
+        // your code goes here
+        if (this.value > other.getValue()) {
+            return 1;
+        } else if (this.value < other.getValue()) {
+            return -1;
+        }
+        if (this.name.toLowerCase() > other.getName().toLowerCase()) {
+            return 1;
+        } else if (this.name.toLowerCase() < other.getName().toLowerCase()) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public use(): void {}
+
+    public toString(): string {
+        return `${this.name} - Value: ${this.value}, Weight: ${this.weight}`;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getValue(): number {
+        return this.value;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getWeight(): number {
+        return this.weight;
+    }
+
+    public setValue(price: number): void {
+        this.value = price;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public setWeight(weight: number): void {
+        this.weight = weight;
+    }
+
+    public static reset(): void {
+        memo.counter = 0;
+    }
 }
--- docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
@@ -1,1 +1,65 @@
+import { Item } from "./Item";
 // your code goes here
+
+export abstract class Weapon extends Item {
+    public static MODIFIER_CHANGE_RATE: number = 0.05;
+    private baseDamage: number = 0;
+    private damageModifier: number = 0;
+    private baseDurability: number = 0;
+    private durabilityModifier: number = 0;
+
+    constructor(name: string, baseDamage: number, baseDurability: number, value: number, weight: number) {
+        super(name, value, weight);
+        this.baseDamage = baseDamage;
+        this.baseDurability = baseDurability;
+    }
+
+    public polish(): void {}
+
+    public use(): string {
+        if (this.getDurability() * 100 <= 0) {
+            return `You can't use the ${this.getName()}, it is broken.`;
+        }
+        const newDurability = (this.getDurability() - Weapon.MODIFIER_CHANGE_RATE) * 100;
+        if (newDurability > 0) {
+            return `You use the ${this.getName()}, dealing ${this.getDamage()} points of damage.`;
+        }
+        return `You use the ${this.getName()}, dealing ${this.getDamage()} points of damage. The ${this.getName()} breaks.`;
+    }
+
+    public toString(): string {
+      return `${this.getName()} - Value: ${this.getValue()}, Weight: ${this.getWeight()}, Damage: ${this.getDamage()}, Durability: ${this.getDurability() * 100}%`;
+    }
+
+    public getBaseDamage(): number {
+        return this.baseDamage;
+    }
+
+    public getBaseDurability(): number {
+        return this.baseDurability;
+    }
+
+    public getDamage(): number {
+        return this.baseDamage + this.damageModifier;
+    }
+
+    public getDurability(): number {
+        return (this.baseDurability + this.durabilityModifier);
+    }
+
+    public getDamageModifier(): number {
+        return this.damageModifier;
+    }
+
+    public setDamageModifier(damageModifier: number): void {
+        this.damageModifier = damageModifier;
+    }
+
+    public getDurabilityModifier(): number {
+        return this.durabilityModifier;
+    }
+
+    public setDurabilityModifier(durabilityModifier: number): void {
+        this.durabilityModifier = durabilityModifier;
+    }
+}
